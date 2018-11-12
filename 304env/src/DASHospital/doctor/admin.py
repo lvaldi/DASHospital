68a8d5dd33615118cb1c813719a014f765e8778d
Huzefa <huzefa.juzer@hotmial.co.uk>
2018-11-12 00:25:53 -0800

    gp and specialist added to UI

--- 304env/src/DASHospital/doctor/admin.py
+++ 304env/src/DASHospital/doctor/admin.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 
 from .models import Patient
 from .models import Doctor
+from .models import Generalpracticioner
+from .models import Specialist
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
+admin.site.register(Generalpracticioner)
+admin.site.register(Specialist)
+
