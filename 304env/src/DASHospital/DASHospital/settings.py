4570a48d5adcff594ac2033ec7039650a29188e8
ItsAdel <adel-s@outlook.com>
2018-11-11 16:18:51 -0800

    Added db config

--- 304env/src/DASHospital/DASHospital/settings.py
+++ 304env/src/DASHospital/DASHospital/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DASHospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'DASHospitalDB',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'Pass123',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
