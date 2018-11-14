c95cbe49665c5cc5a2c5693396e21581de489166
Huzefa <huzefa.juzer@hotmial.co.uk>
2018-11-13 23:24:09 -0800

    Doctor view setup

--- 304env/src/DASHospital/DASHospital/urls.py
+++ 304env/src/DASHospital/DASHospital/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import sign_in_view, create_user_view
-from Staff.views import nurse_detail_view, lab_technician_detail_view
+from Staff.views import nurse_detail_view, lab_technician_detail_view, specialist_detail_view, gp_detail_view
 from WeeklySchedule.views import ScheduledTime_create_view, ScheduledTime_update_view
+from Patient.views import create_patient
 
 urlpatterns = [
     path('', sign_in_view, name='home'),
@@ -27,6 +28,8 @@ urlpatterns = [
     path('Nurse/', nurse_detail_view),
     path('admin/', admin.site.urls),
     path('LabTechnician/', lab_technician_detail_view),
-    path('schedule/<int:wid>/create/', ScheduledTime_create_view.as_view(), name = 'schedule-create')
+    path('schedule/<int:wid>/create/', ScheduledTime_create_view.as_view(), name = 'schedule-create'),
+    path('Specialist/', specialist_detail_view),
+    path('GP/', gp_detail_view)
 
 ]
