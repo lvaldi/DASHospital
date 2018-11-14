ccacd09d8390089682a4db96ea4181c874448859
Darren Fu <darrenfu98@gmail.com>
2018-11-13 22:16:53 -0800

    cleaning up merge conflict

--- 304env/src/DASHospital/DASHospital/urls.py
+++ 304env/src/DASHospital/DASHospital/urls.py
@@ -17,11 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import sign_in_view, create_user_view
 from Staff.views import nurse_detail_view, lab_technician_detail_view
-<<<<<<< HEAD
 from Patient.views import create_patient, profile, account_information
-=======
 from WeeklySchedule.views import Weeklyschedule_create_view
->>>>>>> c290af2eef03809447da97df98afd7b310fdb936
 
 urlpatterns = [
     path('', sign_in_view, name='home'),
@@ -31,8 +28,5 @@ urlpatterns = [
     path('Nurse/', nurse_detail_view),
     path('admin/', admin.site.urls),
     path('LabTechnician/', lab_technician_detail_view),
-<<<<<<< HEAD
-=======
     path('weeklyschedule_create/', Weeklyschedule_create_view)
->>>>>>> c290af2eef03809447da97df98afd7b310fdb936
 ]
